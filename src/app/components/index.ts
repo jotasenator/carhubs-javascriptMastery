074e62286ea09408045aaf9c6ed7947ba37322d0
Maykel <jotasenator@gmail.com>
2023-09-17 21:26:48 -0400

    Implement half part of Cars Model

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,6 +2,12 @@ import CustomButton from "./CustomButton";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from './Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CarDetails from './CarDetails';
+
 
 
 
@@ -10,4 +16,9 @@ export {
     CustomButton,
     Navbar,
     Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,41 @@
-import { Hero } from "./components";
+import { fetchCars } from "@/utils";
+import { Hero, CustomFilter, SearchBar, CarCard } from "./components";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  //console.log(allCars);
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width id='discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div>
+            <h2>Opps, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
